8e629c0b7e8a679fee463d36442bcc1866ba6a00
Eben Goodman <eben.goodman@gmail.com>
2016-09-02 21:42:44 -0400

    WIP design and layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,14 +7,17 @@ import {actions} from '../actions';
 
 export const Login = React.createClass({
   render: function() {
-    return <div className="row">
-      <div className="col-xs-12 col-lg-6 login">
-      </div>
-      <div className="col-xs-12 col-lg-6 login text-xs-center">
-        <h3>Receive update notifications,</h3> 
-        <h3>simply and easily.</h3>
-        <input type="text" name="email" placeholder="Email" className="form-control" />
-        <button type="button" className="btn btn-lg btn-default">Sign Up</button>
+    return <div>
+      <div className="row">
+        <div className="col-xs-12 login text-xs-center">
+          <div className="row">
+            <div className="col-xs-12 col-lg-3" style={{ float: 'none', margin: 'auto' }}>
+              <h3>NPM package notifications, simple and easy.</h3>
+              <input type="text" name="email" placeholder="Email" className="form-control" />
+              <button type="button" className="btn btn-lg btn-default">Set Up</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>;
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,27 @@ export const Main = React.createClass({
         <div className="row row-layout">
           <div className="col-xs-12 col-lg-10 offset-lg-1">
             <div className="row">
-              <div className="col-xs-12 header">
-                NPM Notifier
+              <div className="col-xs-12 col-lg-8 header">
+                <h4>
+                  <img src="img/logo.svg" height="50" />
+                  &nbsp;NPM Notifier
+                </h4>
+              </div>
+              <div className="col-lg-4 header">
+                <ul>
+                  <li><a href="">About</a></li>
+                  <li><a href="">Pricing</a></li>
+                </ul>
               </div>
             </div>
-            <div className="fullheight">
+            <div>
               {this.props.children}
+            </div>
+            <div className="row">
+              <div className="col-xs-12 text-xs-center">
+                <h6>Track 5 packages for free.</h6><h6>Check out pricing for more options.</h6>
+                <span className="footer" style={{ fontSize: '.75em' }}><a href="">pricing</a> | <a href="">terms</a> | <a href="">privacy</a> | <a href="">about</a></span>
+              </div>
             </div>
           </div>
         </div>
