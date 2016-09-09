ef2bab82de9707003f0127adcc70e111aac76968
Eben Goodman <eben.goodman@gmail.com>
2016-09-09 13:02:25 -0400

    WIP chrome autofill issue

--- src/components/LoginEmail.js
+++ src/components/LoginEmail.js
@@ -13,12 +13,13 @@ export const LoginEmail = autofill(React.createClass({
     return this.setState({ email: event.currentTarget.value });
   },
   render: function() {
-    return <form autocomplete="off">
+    return <form>
       <input type="email" 
         name="email" 
         placeholder="Email" 
         className="form-control" 
         value={this.state.email}
+        onBlur={this.handleChange}
         onChange={this.handleChange} />
 
         <button type="button" onClick={() => this.props.login(this.state.email)} className="btn btn-lg btn-default">{this.props.signin ? "Request Sign In Code" : "Set Up"}</button>
