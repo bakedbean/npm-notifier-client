ec6faaa861984093f185e4dc0334234f9947c703
Eben Goodman <eben.goodman@gmail.com>
2016-09-09 11:07:22 -0400

    fix vertical alignment for safari

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ export const Login = React.createClass({
     return <div className="row content">
       <div className="col-xs-12 login text-xs-center">
         <div className="row" style={{ height: '100%' }}>
-          <div className="col-xs-12 col-lg-4" style={{ float: 'none', margin: 'auto' }}>
+          <div className="col-xs-12 col-lg-7" style={{ float: 'none', margin: 'auto' }}>
             {!this.props.auth.get('email') && <h3>NPM package version change notifications, simple and easy.</h3>}
             {!this.props.auth.get('email') && <LoginEmail {...this.props} />}
             {this.props.auth.get('email') && <LoginCode {...this.props} />}
