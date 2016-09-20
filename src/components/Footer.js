17888c1f7c92c5f10453e63699e9375150d378f0
Eben Goodman <eben.goodman@gmail.com>
2016-09-19 21:29:34 -0400

    reduce size of footer when logged in

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import moment from 'moment';
 
 export const Footer = React.createClass({
   render: function() {
-    return <footer className="row hidden-sm-down" id="footer">
+    return <footer className="row hidden-sm-down" id="footer" style={{ height: localStorage.getItem('token') ? '75px' : '100px' }}>
       <div className="col-xs-12 text-xs-center">
         {this.props.account !== 'PAID' && <h6>Track 5 packages for free. Check out pricing for more options.</h6>}
         <span className="footer" style={{ fontSize: '.75em' }}>{this.props.account === 'PAID' ? <Link to="/account">Account</Link> : <Link to="/pricing">Pricing</Link>} | <Link to="/terms">Terms</Link> | <Link to="/privacy">Privacy</Link> | <Link to="/about">About</Link></span>
