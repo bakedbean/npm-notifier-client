0121548c0e5bf9acd9c921442623e311f4784863
Eben Goodman <eben.goodman@gmail.com>
2016-09-22 14:27:39 -0400

    remove free option from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,8 @@ import moment from 'moment';
 
 export const Footer = React.createClass({
   render: function() {
-    return <footer className="row hidden-sm-down" id="footer" style={{ height: localStorage.getItem('token') ? '75px' : '100px' }}>
+    return <footer className="row hidden-sm-down" id="footer" style={{ height: '75px' }}>
       <div className="col-xs-12 text-xs-center">
-        {this.props.account !== 'PAID' && <h6>Track 5 packages for free. Check out pricing for more options.</h6>}
         <span className="footer" style={{ fontSize: '.75em' }}>{this.props.account === 'PAID' ? <Link to="/account">Account</Link> : <Link to="/pricing">Pricing</Link>} | <Link to="/terms">Terms</Link> | <Link to="/privacy">Privacy</Link> | <Link to="/about">About</Link></span>
         {this.props.account === 'PAID' && <div style={{ fontSize: '.75em' }}>Copyright NPM Notifier {moment().format('YYYY')}</div>}
       </div>
