a3c4d6fab6b8dfc9f285d0b1cf4d92cf3093a4ce
Eben Goodman <eben.goodman@gmail.com>
2016-10-19 21:35:01 -0400

    add ga tracking

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ var config = getConfig({
     return {
       'index.html': context.defaultTemplate({
         title: 'NPM Notifier',
-        head: "<meta name='description' content='NPM Package version change notifications' /><link rel='author' href='/humans.txt' /><link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'><script src='https://checkout.stripe.com/checkout.js'></script>"
+        head: "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-85986360-1', 'auto');ga('send', 'pageview');</script><meta name='description' content='NPM Package version change notifications' /><link rel='author' href='/humans.txt' /><link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'><script src='https://checkout.stripe.com/checkout.js'></script>"
       })
     }
   },
